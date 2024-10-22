cfa71d4483005384e762695b174c6390a9cea9ac
Prescaa <145347108+Prescaa@users.noreply.github.com>
2024-10-22 21:14:54 +0800

    Add files via upload

---
+++ Praktikum_Modul2/praktikum2.soal3/Soal3Main.java
@@ -0,0 +1,24 @@
+package Soal3_Praktikum;
+
+public class Soal3Main {
+	public static void main(String[] args) { 
+    Pegawai p1 = new Pegawai();
+    
+    //terjadi error karena tidak ada titik koma (;)
+    //p1.nama = "Roi"
+    p1.nama = "Roi";
+    p1.asal = "Kingdom of Orvel";
+    p1.setJabatan("Assasin"); 
+    
+    //inisiasi nilai "17" pada variabel "umur", karena sebelumnya tidak ditetapkan
+    p1.umur = 17;
+    
+    System.out.println("Nama Pegawai: " + p1.getNama()); 
+    System.out.println("Asal: " + p1.getAsal()); 
+    System.out.println("Jabatan: " + p1.jabatan);
+    
+    //Menambah " tahun" pada bagian ("Umur: " + p1.umur), agar sesuai dengan output yang diminta
+    //System.out.println("Umur: " + p1.umur); 
+    System.out.println("Umur: " + p1.umur + " tahun"); 
+    }
+}

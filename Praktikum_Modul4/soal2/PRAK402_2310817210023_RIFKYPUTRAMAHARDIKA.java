3a2bf01f83d2d2cb778bef10ab26ee1e7c5c3f2d
Prescaa <145347108+Prescaa@users.noreply.github.com>
2024-12-07 22:17:54 +0800

    Update and rename Main.java to PRAK402_2310817210023_RIFKYPUTRAMAHARDIKA.java

--- Praktikum_Modul4/soal2/PRAK402_2310817210023_RIFKYPUTRAMAHARDIKA.java
+++ Praktikum_Modul4/soal2/PRAK402_2310817210023_RIFKYPUTRAMAHARDIKA.java
@@ -2,7 +2,7 @@ package soal2;
 
 import java.util.Scanner;
 
-public class Main {
+public class PRAK402_2310817210023_RIFKYPUTRAMAHARDIKA {
 	public static void main (String args[]) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Pilih jenis hewan yang ingin diinputkan: ");

84f377763b3ceb17a332044dfa1f0fdf3e7e2398
Prescaa <rifkymahardika938@gmail.com>
2025-04-25 23:21:04 +0800

    Update TipCalculator.kt

--- Praktikum Mobile/MODUL 2/Modul2/app/src/main/java/com/presca/modul2/TipCalculator.kt
+++ Praktikum Mobile/MODUL 2/Modul2/app/src/main/java/com/presca/modul2/TipCalculator.kt
@@ -6,6 +6,6 @@ object TipCalculator {
     fun calculateTip(cost: Double, percentage: Int, roundUp: Boolean): Double {
         var tip = cost * percentage / 100
         if (roundUp) tip = ceil(tip)
-        return tippp
+        return tip
     }
 }

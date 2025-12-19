bcbef718d27a166c077641329fcfcaecfd71f341
Rifky Putra M. <rifkymahardika938@gmail.com>
2025-12-19 11:56:27 +0800

    Delete toyota_bi_system directory

--- toyota_bi_system/dwh/init.sql
+++
@@ -1,8 +0,0 @@
--- Create extensions
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
--- Create schema for Toyota data
-CREATE SCHEMA IF NOT EXISTS toyota_dw;
-
--- Set search path
-SET search_path TO toyota_dw, public;
